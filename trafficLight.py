72f539889dd089a6a452ab47942054b858734e5e
lagosyusuf <lagosyusuf@gmail.com>
2024-11-03 21:17:56 -0800

    Initial commit for the traffic light implementation

---
+++ trafficLight.py
@@ -0,0 +1,8 @@
+light = input("Which color is the light indication? ")
+if light == "Green":
+    print("Please keep going")
+elif light == "Yellow":
+    print("Please go slow")
+else:
+    print("Please stop!")
+        # comment: 
